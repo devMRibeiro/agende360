914e0c917c33451d21bccc368cf560a7173da9b8
devMRibeiro <michael.douglas2020@hotmail.com>
2026-03-23 18:31:10 -0300

    chore: WIP - sync progress

--- src/main/resources/db/migration/V3__create_table_customer.sql
+++ src/main/resources/db/migration/V3__create_table_customer.sql
@@ -1,15 +1,13 @@
 CREATE TABLE customer (
     id          UUID            NOT NULL DEFAULT gen_random_uuid(),
+    name 		VARCHAR(100)    NOT NULL,
     phone       VARCHAR(255)    NOT NULL,
-    email       VARCHAR(255)    NOT NULL,
-    company_id  UUID,
+    email       VARCHAR(255),
     created_at  TIMESTAMP       NOT NULL,
     updated_at  TIMESTAMP       NOT NULL,
 
     CONSTRAINT pk_customer PRIMARY KEY (id),
-    CONSTRAINT uq_customer_email UNIQUE (email),
-    CONSTRAINT fk_customer_company FOREIGN KEY (company_id) REFERENCES company (id)
+	CONSTRAINT uq_customer_phone UNIQUE (phone)
 );
 
-CREATE INDEX idx_customer_phone ON customer (phone);
-CREATE UNIQUE INDEX idx_customer_email ON customer (email);
+CREATE UNIQUE INDEX idx_customer_phone ON customer (phone);

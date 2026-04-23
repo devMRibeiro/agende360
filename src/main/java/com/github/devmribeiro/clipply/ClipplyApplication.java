9023ef82e96bf4710deb64b82c74d2caef0f535c
devMRibeiro <michael.douglas2020@hotmail.com>
2026-04-23 19:37:36 -0300

    feat: set default timezone to America/Sao_Paulo

--- src/main/java/com/github/devmribeiro/clipply/ClipplyApplication.java
+++ src/main/java/com/github/devmribeiro/clipply/ClipplyApplication.java
@@ -1,5 +1,7 @@
 package com.github.devmribeiro.clipply;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 public class ClipplyApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 		SpringApplication.run(ClipplyApplication.class, args);
 	}
 }

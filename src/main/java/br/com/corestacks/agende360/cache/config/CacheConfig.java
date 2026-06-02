97b4b0435c6e93f6d0e274b28d2cd38a268f26e9
Michael Ribeiro <michael.ribeiro@allcom.local>
2026-06-02 18:03:23 -0300

    feat: adiciona configuracao de cache para Company e altera o tipo de objeto de usersCache

--- src/main/java/br/com/corestacks/agende360/cache/config/CacheConfig.java
+++ src/main/java/br/com/corestacks/agende360/cache/config/CacheConfig.java
@@ -1,7 +1,7 @@
 package br.com.corestacks.agende360.cache.config;
 
-import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.context.annotation.Bean;
@@ -10,16 +10,42 @@ import org.springframework.context.annotation.Configuration;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
-import br.com.corestacks.agende360.application.dto.response.ProductResponse;
+import br.com.corestacks.agende360.application.model.Company;
+import br.com.corestacks.agende360.application.model.CompanySettings;
+import br.com.corestacks.agende360.application.model.Product;
+import br.com.corestacks.agende360.application.model.User;
 
 @Configuration
 public class CacheConfig {
 	
 	@Bean
-	public Cache<UUID, List<ProductResponse>> servicesCache() {
+	public Cache<UUID, List<Product>> productsCache() {
 		return Caffeine.newBuilder()
 				.maximumSize(10000)
-				.expireAfterWrite(Duration.ofHours(1))
+				.recordStats()
+				.build();
+	}
+	
+	@Bean
+	public Cache<UUID, Map<UUID, User>> usersCache() {
+		return Caffeine.newBuilder()
+				.maximumSize(10000)
+				.recordStats()
+				.build();
+	}
+
+	@Bean
+	public Cache<String, Company> companysCache() {
+		return Caffeine.newBuilder()
+				.maximumSize(10000)
+				.recordStats()
+				.build();
+	}
+
+	@Bean
+	public Cache<UUID, CompanySettings> companySettings() {
+		return Caffeine.newBuilder()
+				.maximumSize(10000)
 				.recordStats()
 				.build();
 	}

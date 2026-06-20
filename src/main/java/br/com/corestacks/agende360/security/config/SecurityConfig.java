6bad6de879ecbc3a4ed1f87189cb49d3f7d14d9b
devMRibeiro <michael.douglas2020@hotmail.com>
2026-06-20 17:11:56 -0300

    fix: permite requisições para webhook meta

--- src/main/java/br/com/corestacks/agende360/security/config/SecurityConfig.java
+++ src/main/java/br/com/corestacks/agende360/security/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
 	        	    .requestMatchers("/api/auth/**").permitAll()
 	        	    .requestMatchers("/api/public/**").permitAll()
 	        	    .requestMatchers("/api/agende360/**").hasRole(UserRole.SUPPORT.name())
-//	        	    .requestMatchers("/api/billing/webhook").permitAll()
+	        	    .requestMatchers("/api/webhook/meta").permitAll()
 	        	    .anyRequest().authenticated()
         	)
 	        .authenticationProvider(authenticationProvider())

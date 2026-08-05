6e38c4c4c1a2695ab41dbe5377dfc464d4f5fcb0
devMRibeiro <michael.douglas2020@hotmail.com>
2026-08-05 03:17:02 -0300

    refactor: otimiza resolução de handlers do outbox

--- src/main/java/br/com/corestacks/agende360/outbox/publisher/OutboxPublisherService.java
+++ src/main/java/br/com/corestacks/agende360/outbox/publisher/OutboxPublisherService.java
@@ -1,58 +1,37 @@
 package br.com.corestacks.agende360.outbox.publisher;
 
-import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import br.com.corestacks.agende360.outbox.enums.OutboxEventType;
 import br.com.corestacks.agende360.outbox.handler.OutboxEventHandler;
 import br.com.corestacks.agende360.outbox.model.OutboxEvent;
-import br.com.corestacks.agende360.outbox.model.OutboxStatus;
-import br.com.corestacks.agende360.outbox.repository.OutboxEventRepository;
 
 @Service
 public class OutboxPublisherService {
 
-	private final List<OutboxEventHandler> handlers;
-	private final OutboxEventRepository outboxEventRepository;
+    private final Map<OutboxEventType, OutboxEventHandler> mapHandlers;
 
-	public OutboxPublisherService(
-			List<OutboxEventHandler> handlers,
-			OutboxEventRepository outboxEventRepository) {
-		this.outboxEventRepository = outboxEventRepository;
-		this.handlers = handlers;
-	}
+    public OutboxPublisherService(List<OutboxEventHandler> handlers) {
 
-	public void publish(OutboxEvent event) {
-    	OutboxEventHandler found = null;
+    	Map<OutboxEventType, OutboxEventHandler> map = new HashMap<OutboxEventType, OutboxEventHandler>();
 
-    	for (OutboxEventHandler handler : handlers) {
-    	    if (handler.supports(event.getEventType())) {
-    	        found = handler;
-    	        break;
-    	    }
-    	}
+    	for (OutboxEventHandler handler : handlers)
+    	    map.put(handler.supports(), handler);
 
-    	if (found == null)
-    	    throw new IllegalStateException("Nenhum handler encontrado para " + event.getEventType());
-
-    	try {
-    		
-    		found.handle(event);
-    		
-    		event.setEventStatus(OutboxStatus.PROCESSED);
-    	    event.setSentAt(Instant.now());
-    		
-    	} catch (Exception e) {
-            int retry = event.getRetryCount() + 1;
-            event.setRetryCount(retry);
-            event.setNextAttemptAt(Instant.now().plusSeconds(calculateBackoff(retry)));
-        }
-
-    	outboxEventRepository.save(event);
+    	this.mapHandlers = map;
     }
-	
-	private long calculateBackoff(int retryCount) {
-	    return Math.min((long) Math.pow(2, retryCount) * 30, 3600);
-	}
+
+    public void publish(OutboxEvent event) {
+
+        OutboxEventHandler handler = mapHandlers.get(event.getEventType());
+
+        if (handler == null)
+            throw new IllegalStateException("Nenhum handler encontrado para " + event.getEventType());
+
+        handler.handle(event);
+    }
 }
